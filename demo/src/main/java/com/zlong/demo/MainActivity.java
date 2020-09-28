e961608a072944ba436b3e5df1f72a28a8c29d75
zoulong <839169106@qq.com>
2020-09-28 19:50:05 +0800

    .

--- demo/src/main/java/com/zlong/demo/MainActivity.java
+++ demo/src/main/java/com/zlong/demo/MainActivity.java
@@ -1,13 +1,9 @@
 package com.zlong.demo;
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 
-import androidx.annotation.IdRes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.zlong.skinpeeler.SkinPeeler;
